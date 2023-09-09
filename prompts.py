e182e722b1cac9619fa2473d738c7543f33b334d
aleburbridge <aleburbridge@gmail.com>
2023-09-08 21:49:51 -0700

    stuff

--- prompts.py
+++ prompts.py
@@ -37,9 +37,9 @@ prompts = {
             {
                 "speaker": "cohost",
                 "text": "That's right {@host}, We are also being told {!main_story_info_2}",
-                "prompt": {
+                "prompts": {
                     "id": "main_story_info2",
-                    "description": "Details about the story, {!main_story}. We also know {!main_story_info}."
+                    "description": "Details about the story, {main_story}. What we know so far: {main_story_info}."
                 }
             }
         ],
@@ -50,11 +50,16 @@ prompts = {
         "lines": [
             {
                 "speaker": "host",
-                "text": "This just in, I'm getting word that we've managed to get an exclusive interview with Detective {@detective} Gumshoe who is live on the scene. Detective, what can you share with us?"
+                "text": "This just in, I'm getting word that we've managed to get an exclusive interview with Detective {detective} Gumshoe who is live on the scene. Detective, what can you share with us?"
             },
             {
                 "speaker": "detective",
-                "text": "Well, the situation is worse than we thought, my team has just discovered that {!detective_info1}."
+                "text": "Well, the situation is worse than we thought, my team has just discovered that {detective_info1}.",
+                "propmts":  {
+                    "id": "detective_info1",
+                    "description": "Write a discovery for the detective to present. Context: 'Well, the situation is worse than we thought. My team has just discovered that (Your text here)."
+                },
+                
             },
             {
                 "speaker": "host",
@@ -62,7 +67,11 @@ prompts = {
             },
             {
                 "speaker": "detective",
-                "text": "It gets worse. In all my years as a detective never before have I seen {!detective_info2}."
+                "text": "It gets worse. In all my years as a detective never before have I seen {detective_info2}.",
+                "prompts":   {
+                    "id": "detective_info2",
+                    "description": "Write a discovery for the detective to present. Context: 'It gets worse, in all my years as a detective never before have I seen (your text here)"
+                },
             },
             {
                 "speaker": "cohost",
@@ -70,27 +79,17 @@ prompts = {
             },
             {
                 "speaker": "detective",
-                "text": "Indeed I do. A note was discovered at the scene of the incident, it reads: {!detective_note}."
+                "text": "Indeed I do. A note was discovered at the scene of the incident, it reads: {detective_note}.",
+                "prompts": {
+                    "id": "detective_note",
+                    "description": "The detective will present a note that was discovered at the scene. Write what the note says."
+                }
             },
             {
                 "speaker": "host",
                 "text": "Detective, thank you for your time."
             }
         ],
-        "prompts": [
-        {
-            "id": "detective_info1",
-            "description": "Write a discovery for the detective to present. Context: 'Well, the situation is worse than we thought. My team has just discovered that (Your text here)."
-        },
-        {
-            "id": "detective_info2",
-            "description": "Write a discovery for the detective to present. Context: 'It gets worse, in all my years as a detective never before have I seen (your text here)"
-        },
-        {
-            "id": "detective_note",
-            "description": "The detective will present a note that was discovered at the scene. Write what the note says."
-        }
-        ]
     },
     
 }
