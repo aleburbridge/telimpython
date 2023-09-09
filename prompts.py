efe46f89706a38b35ccef09cda0cab0ee98aa974
aleburbridge <aleburbridge@gmail.com>
2023-09-09 14:14:02 -0700

    prompt

--- prompts.py
+++ prompts.py
@@ -91,5 +91,52 @@ prompts = {
             }
         ],
     },
+    "crime_conclusion_1": {
+        "tags": ["segment", "crime"],
+        "dependencies": [],
+        "lines": [
+            {
+                "speaker": "host",
+                "text": "This just in, I'm getting word that we've managed to get an exclusive interview with Detective {detective} Gumshoe who is live on the scene. Detective, what can you share with us?"
+            },
+            {
+                "speaker": "detective",
+                "text": "Well, the situation is worse than we thought, my team has just discovered that {detective_info1}.",
+                "propmts":  {
+                    "id": "detective_info1",
+                    "description": "Write a discovery for the detective to present. Context: 'Well, the situation is worse than we thought. My team has just discovered that (Your text here)."
+                },
+                
+            },
+            {
+                "speaker": "host",
+                "text": "Horrific."
+            },
+            {
+                "speaker": "detective",
+                "text": "It gets worse. In all my years as a detective never before have I seen {detective_info2}.",
+                "prompts":   {
+                    "id": "detective_info2",
+                    "description": "Write a discovery for the detective to present. Context: 'It gets worse, in all my years as a detective never before have I seen (your text here)"
+                },
+            },
+            {
+                "speaker": "cohost",
+                "text": "Well detective, do you have any clues as to why this happened?"
+            },
+            {
+                "speaker": "detective",
+                "text": "Indeed I do. A note was discovered at the scene of the incident, it reads: {detective_note}.",
+                "prompts": {
+                    "id": "detective_note",
+                    "description": "The detective will present a note that was discovered at the scene. Write what the note says."
+                }
+            },
+            {
+                "speaker": "host",
+                "text": "Detective, thank you for your time."
+            }
+        ],
+    },
     
 }
