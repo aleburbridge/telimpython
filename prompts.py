0134072be88bd55e215bb04bbb4c851fbf30e891
aleburbridge <aleburbridge@gmail.com>
2023-09-09 21:59:57 -0700

    commit

--- prompts.py
+++ prompts.py
@@ -12,36 +12,32 @@ prompts = {
     # GENERAL ---------------------------------------------------------------------------------
     # CRIME -----------------------------------------------------------------------------------
     "crime_intro_1": {
-        "tags": ["introduction","crime"],
+        "tags": ["introduction", "crime"],
         "lines": [
-            {
-                "speaker": "host",
-                "text": "I'm {host} {host_lastname} with Telimpropmtu news.",
-            },
-            {
-                "speaker": "cohost",
-                "text": "And I'm {cohost} {cohost_lastname}",
-            },
+            {"speaker": "host", "text": "I'm {host} {host_lastname} with Telimpropmtu news."},
+            {"speaker": "cohost", "text": "And I'm {cohost} {cohost_lastname}"},
             {
                 "speaker": "host",
                 "text": "Our leading story tonight, {main_story}. Here's what we know so far: {main_story_info}",
-                "prompts": [
-                    {
-                        "id": "main_story",
-                        "description": "Initial details about the story, {main_story}"
-                    },
-                    {
-                        "id": "main_story_info",
-                        "description": "Initial details about the story, {main_story}"
-                    }
-                ]
+                "prompts": {
+                    "id": "main_story",
+                    "description": "Initial details about the story."
+                }
+            },
+            {
+                "speaker": "host",
+                "text": "{main_story_info}",
+                "prompts": {
+                    "id": "main_story_info",
+                    "description": "Details about the leading story."
+                }
             },
             {
                 "speaker": "cohost",
-                "text": "That's right {@host}, We are also being told {!main_story_info_2}",
+                "text": "That's right {@host}, We are also being told {main_story_info_2}",
                 "prompts": {
-                    "id": "main_story_info2",
-                    "description": "Details about the story, {main_story}. What we know so far: {main_story_info}."
+                    "id": "main_story_info_2",
+                    "description": "More details about the story."
                 }
             }
         ],
@@ -56,23 +52,19 @@ prompts = {
             {
                 "speaker": "detective",
                 "text": "Well, the situation is worse than we thought, my team has just discovered that {detective_info1}.",
-                "propmts":  {
+                "prompts": {
                     "id": "detective_info1",
-                    "description": "Write a discovery for the detective to present. Context: 'Well, the situation is worse than we thought. My team has just discovered that (Your text here)."
-                },
-                
+                    "description": "Write a discovery for the detective to present."
+                }
             },
-            {
-                "speaker": "host",
-                "text": "Horrific."
-            },
+            {"speaker": "host", "text": "Horrific."},
             {
                 "speaker": "detective",
                 "text": "It gets worse. In all my years as a detective never before have I seen {detective_info2}.",
-                "prompts":   {
+                "prompts": {
                     "id": "detective_info2",
-                    "description": "Write a discovery for the detective to present. Context: 'It gets worse, in all my years as a detective never before have I seen (your text here)"
-                },
+                    "description": "Write a discovery for the detective to present."
+                }
             },
             {
                 "speaker": "cohost",
@@ -83,42 +75,32 @@ prompts = {
                 "text": "Indeed I do. A note was discovered at the scene of the incident, it reads: {detective_note}.",
                 "prompts": {
                     "id": "detective_note",
-                    "description": "The detective will present a note that was discovered at the scene. Write what the note says."
+                    "description": "The detective will present a note that was discovered at the scene."
                 }
             },
-            {
-                "speaker": "host",
-                "text": "Detective, thank you for your time."
-            }
+            {"speaker": "host", "text": "Detective, thank you for your time."}
         ],
     },
     "crime_conclusion_1": {
-        "tags": ["conclusion"],
+        "tags": ["conclusion", "crime"],
         "lines": [
             {
-                "speaker": "HOST",
-                "text": "Well, there you have it folks. A story like this you only see once in a lifetime. {!host_story_description} {@cohost} its an unbelievable story isn't it?",
-                "prompts": [
-                    {
-                        "id": "host_story_description",
-                        "description": "A few words for the closing remarks of the story\nContext: \"Well, there you have it folks. A story like this you only see once in a lifetime. (Your text here)\""
-                    }
-                ]
+                "speaker": "host",
+                "text": "Well, there you have it folks. A story like this you only see once in a lifetime. {host_story_description} {cohost}, its an unbelievable story isn't it?",
+                "prompts": {
+                    "id": "host_story_description",
+                    "description": "A few words for the closing remarks of the story."
+                }
             },
             {
-                "speaker": "COHOST",
-                "text": "Well, it shouldn't be all that surprising given that {!cohost_statistic}.",
-                "prompts": [
-                    {
-                        "id": "cohost_statistic",
-                        "description": "A fake fact or statistic for the CoHost to present\nContext: \"Perhaps this story shouldn't be all that surprising given that (Your text here)\""
-                    }
-                ]
+                "speaker": "cohost",
+                "text": "Well, it shouldn't be all that surprising given that {cohost_statistic}.",
+                "prompts": {
+                    "id": "cohost_statistic",
+                    "description": "A fake fact or statistic for the CoHost to present."
+                }
             },
-            {
-                "speaker": "HOST",
-                "text": "Well folks, that's all the time we have for tonight. Goodbye."
-            }
+            {"speaker": "host", "text": "Well folks, that's all the time we have for tonight. Goodbye."}
         ]
     },
     # POLITICS -----------------------------------------------------------------------------------
